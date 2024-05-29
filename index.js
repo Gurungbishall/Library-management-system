addadcd8f0cfdb15e789531ddea283055bcb3ad1
mee <mee@kali.Kali>
2024-05-29 20:24:48 +0545

    change5

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const btn = document.getElementById("btn");
-const optionbox = document.getElementById("search_option");
 
 btn.addEventListener("click", () => {
-    optionbox.classList.toggle('active')
+  const optionbox = document.getElementById("search_option");
+  if (optionbox.style.display == "none") {
+    optionbox.style.cssText = "display:grid; gap: 5px";
+  } else {
+    optionbox.style.cssText = "display: none";
+  }
 });
 
+const user_btn = document.getElementById("user");
+
+user_btn.addEventListener("click", () => {
+  const user_option = document.getElementById("user_option");
+  if (user_option.style.display == "none") {
+    user_option.style.cssText = "display:grid; gap: 5px";
+  } else {
+    user_option.style.cssText = "display:none";
+  }
+});
